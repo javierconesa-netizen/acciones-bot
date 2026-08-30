7e05ed009f079a435a495c1c178249b93438c008
javierconesa-netizen <javier.conesa@gmail.com>
2026-08-30 09:57:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from datetime import datetime
+import subprocess
+import sys
+
+try:
+  import yfinance as yf
+except ImportError:
+  subprocess.check_call([sys.executable, "-m", "pip", "install", "yfinance"])
+  import yfinance as yf
+    from datetime import datetime
 import pandas as pd
 import streamlit as st
 import yfinance as yf
